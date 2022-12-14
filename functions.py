88b846ef50c739feae0e0c543ed7d3b8cd1a6d75
TwigaX <j.k.reszka@gmail.com>
2022-12-14 14:20:56 +0100

    Create static web interface

--- functions.py
+++ functions.py
@@ -1,12 +1,12 @@
 # good practice save functions in separate file
-def get_todos(filepath = "txtfiles/Todolist.txt"):
+def get_todos(filepath = "App1/txtfiles/Todolist.txt"):
     """ Read the txt file and return list of to-do items""" #DOCSTRINGS
     with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
 
-def write_todos(todos_arg, filepath = "txtfiles/Todolist.txt"):
+def write_todos(todos_arg, filepath = "App1/txtfiles/Todolist.txt"):
     """ Write the to- do items in the txt file"""
     with open(filepath, 'w') as file_local:
         file_local.writelines(todos_arg)
